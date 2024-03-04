18969134d50c7e172e204b3f921c9bac4446a2d7
Emily Tran Mileva <emily.tran.mileva@gmail.com>
2024-03-03 18:41:47 -0800

    Deploy command by daddy.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,1 @@
-ssh todor@milevi.org && \
-cd courses_calculator/calculator/public_html/solar && \
-git pull && \
-exit
+ssh todor@milevi.org "cd courses_calculator/calculator/public_html/solar && git pull && exit"
